6adbd2dec09702e4407ec9079bc0338656af94d2
TJ Momon <tjmomon92@gmail.com>
2023-04-29 12:42:31 -0500

    added in spoof data to data.js file

--- Develop/utils/data.js
+++ Develop/utils/data.js
@@ -1,13 +1,59 @@
 const names = [
-  
+  'Aaron',
+  'Cameron',
+  'TJ',
+  'Adrian',
+  'Amy',
+  'Charles',
+  'Christopher',
+  'CJ',
+  'Chris',
+  'Cristobal',
+  'Diem',
+  'Dillon',
+  'Gabriel',
+  'Giuliana',
+  'Jacob',
+  'Jerrod',
+  'Jonathan',
+  'Johnathon',
+  'Jose',
+  'Joshua',
+  'Julian',
+  'Kiefer',
+  'Lemry',
+  'Luis',
+  'Soa',
+  'Taylor',
+  'Tom',
+  'Tony',
+  'Trey',
+  'Valerie',
 ];
 
 const thoughts = [
-
+  'Hello World',
+  'Lorem-Fucking-Ipsum',
+  'This code will totally change your life',
+  'New Movie Trailer is fire',
+  'New Movie Trailer is shit',
+  'New Movie Trailers are just mediums for capitalist control',
+  'Hur Dur Democrats',
+  'Hur Dur Republicans',
+  'How to do that one thing that totally takes years of practice and be a complete expert in .02 seconds Im so serial dont click away',
 ];
 
 const reactions = [
-
+  'WTF',
+  'But why tho',
+  'Ew what is that',
+  'I love this more than my own parents',
+  'This is the Steven Seagal of opinions',
+  'LQTM',
+  'That actually was not terrible',
+  'That actually was terrible',
+  'I tried this, and my dog divorced me',
+  'Lorem-Fucking-Ipsum'
 ];
 
 const users = [];

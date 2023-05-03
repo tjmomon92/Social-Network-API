50165080d9a167d3f2e57841590259daf240eb60
TJ Momon <tjmomon92@gmail.com>
2023-05-02 20:22:18 -0500

    bug troubleshooting, cannot get data in Insomnia

--- Develop/models/Users.js
+++ Develop/models/Users.js
@@ -17,7 +17,7 @@ const userSchema = new Schema(
       type: String,
       unique: true,
       required: true,
-      validate: [validateEmail, 'Please fill a valid email address'],
+      validate: [validateEmail, 'Please use a valid email address'],
       match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
     },
     thoughts: [
